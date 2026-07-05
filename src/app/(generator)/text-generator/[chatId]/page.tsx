b966e71f8347ec6a2cecf78e5ccabbed51752ab9
Anchor-techno-soft <aswinkumar@anchortechnosoft.com>
2026-07-05 23:53:54 +0530

    fix: compatible useChat implementation for deployed SDK version

--- src/app/(generator)/text-generator/[chatId]/page.tsx
+++ src/app/(generator)/text-generator/[chatId]/page.tsx
@@ -8,31 +8,10 @@ import { createIdGenerator } from 'ai';
 import { useState } from 'react';
 
 export default function Page() {
-  // const [isThinking, setIsThinking] = useState(false);
-
-  // const chatHandler = useChat({
-  //   generateId: createIdGenerator({ prefix: 'msgc' }),
-  // });
-
-  // const handleSubmit = (e: React.FormEvent) => {
-  //   setIsThinking(true);
-  //   chatHandler.handleSubmit(e);
-  // };
-
+ 
   return (
     <div className="contents">
-      {/* <RenderMessage useChat={chatHandler} isThinking={isThinking} />
-
-      <div className="px-5 md:px-12">
-        <form onSubmit={handleSubmit}>
-          <GeneratorInput
-            value={chatHandler.input}
-            onChange={chatHandler.handleInputChange}
-          />
-        </form>
-
-        <GradientBlob />
-      </div> */}
+     
     </div>
   );
 }
