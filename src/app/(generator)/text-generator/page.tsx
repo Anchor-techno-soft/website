c0c5c57cad889ddc1044dde69b9554e0920d644a
Anchor-techno-soft <aswinkumar@anchortechnosoft.com>
2026-07-06 00:16:49 +0530

    remove unused generator page

--- src/app/(generator)/text-generator/page.tsx
+++ src/app/(generator)/text-generator/page.tsx
@@ -1,25 +1,3 @@
-'use client';
-
-import GeneratorInput from '@/components/generator/generator-input';
-import { RenderMessage } from '@/components/generator/render-message';
-import { GradientBlob } from '@/components/gradient-blob';
-import { useChat } from '@ai-sdk/react';
-import { createIdGenerator } from 'ai';
-import { useState } from 'react';
-
 export default function Page() {
-  const [isThinking, setIsThinking] = useState(false);
-
-  const chatHandler = useChat({
-    generateId: createIdGenerator({ prefix: 'msgc' }),
-    onFinish: () => setIsThinking(false), // ✅ correct replacement
-  });
-
-  return (
-    <div className="contents">
-      <RenderMessage useChat={chatHandler} isThinking={isThinking} />
-
-     
-    </div>
-  );
+  return null;
 }
